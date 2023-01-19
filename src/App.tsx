c7c5fce5a591f1244c20257d3f4aafa2f419e9fd
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-01-20 05:50:33 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-black h-screen">
+      <div className="flex justify-center p-5">
+        <h1 className="text-white">Tailwindcss Ready</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
