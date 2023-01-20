a006b0457ab8db6473303f51e179d25a116a32a0
Herdy <54350341+herdyhardiyant@users.noreply.github.com>
2023-01-20 21:46:19 +0700

    Room list

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
 import { AiOutlineSearch } from 'react-icons/ai';
-import {IoSearchCircle} from 'react-icons/io5'
+import { IoSearchCircle } from 'react-icons/io5'
+import HouseItem from './components/HouseItem';
 
 function App() {
 
+  const houseList = [];
+
+  for (let i = 0; i < 20; i++) {
+    houseList.push(<HouseItem />
+    );
+  }
+
+
   return (
-    <div className="bg-black h-screen">
+    <div className=" flex flex-col bg-black h-min">
+
       {/* Navbar start */}
-      <div className="flex justify-around items-center p-5 text-white h-fit">
-        <h1 className="text-red-500">Rent House</h1>
+      <div className="flex justify-around items-center p-5 text-white w-full">
+        <h1 className="text-red-500 text-xl font-bold">Red House</h1>
         <div className="flex justify-around place-items-center border-solid border border-red-600 rounded-full p-1 pl-6 pr-2 ">
-          <h3>Anywhere</h3>
+          <p>Anywhere</p>
           <div className="border border-solid border-l-white min-h-full h-7 mx-4"></div>
-          <h3>Anyweek</h3>
+          <p>Anyweek</p>
           <div className="border border-solid border-l-white min-h-full h-7 mx-4"></div>
-          <h3>Add guest</h3>
-          <IoSearchCircle className="ml-1" size="40"/>
+          <p>Add guest</p>
+          <IoSearchCircle className="ml-1" size="40" />
         </div>
-        <h1>Account</h1>
+        <h1 className='text-xl'>Account</h1>
       </div>
       {/* Navbar Ends */}
 
-
+      {/* House list */}
+      <div className='flex flex-wrap content-evenly justify-center w-full'>
+        {
+          houseList
+        }
+      </div>
+      {/* House List */}
 
     </div>
   )
 }
+
+
 
 export default App
---
+++ src/components/HouseItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HouseItem({ }) {
+  return (<div className='text-white m-2 mb-10 '>
+    <div className='h-52 w-64 bg-slate-600 rounded-lg'>
+    </div>
+    <h1>Beach, Indonesia</h1>
+    <h3>Rp5,700,000 night</h3>
+  </div>);
+}
+
+export default HouseItem;
